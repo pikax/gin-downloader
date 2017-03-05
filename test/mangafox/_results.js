b505f3d09646da9640253e7bea8e0b028e8e00f5
pikax <david-181@hotmail.com>
2017-03-05 00:29:08 +0000

    adding more tests to mangafox

--- test/mangafox/_results.js
+++ test/mangafox/_results.js
@@ -2,10 +2,32 @@
  * Created by rodriguesc on 02/03/2017.
  */
 
+let manga = {
+  image: 'http://h.mfcdn.net/store/manga/551/cover.jpg',
+	title: 'Gintama',
+	csv_title: 'กินทามะ; 銀魂; 银魂; 긴타마; 은혼; Gin Tama; Silver Soul; غينتاما - الروح الفضيه; Сребърна душа (Bulgarian); Gümüş Ruh (Turkish); Jiwa Perak; Серебряная душа (Russian)',
+	released: '2003',
+	authors: [ 'Sorachi Hideaki' ],
+	artists: [ 'Sorachi Hideaki' ],
+	genres:
+		[ 'Action',
+			'Adventure',
+			'Comedy',
+			'Drama',
+			'Historical',
+			'Sci-fi',
+			'Shounen',
+			'Supernatural' ]
+};
+
+
+
 export default {
   mangas_count : 18671,
-  manga_name : "Gintama",
+  manga_name : manga.title,
 
     chapter_count : 55,
+
+  manga
 
 }
