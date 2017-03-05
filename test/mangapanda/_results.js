bb51709c425a6bed751ca2e4ce8e881d21564c19
pikax <david-181@hotmail.com>
2017-03-05 01:32:04 +0000

    add mangapanda parser and tests

---
+++ test/mangapanda/_results.js
@@ -0,0 +1,31 @@
+/**
+ * Created by rodriguesc on 02/03/2017.
+ */
+
+let manga = {
+  image: 'http://s5.mangapanda.com/cover/gintama/gintama-l0.jpg',
+	title: 'Gintama',
+	csv_title: '',
+	released: '2003',
+	status : 'Ongoing',
+	author: 'Sorachi, Hideaki',
+	artist: 'Sorachi, Hideaki' ,
+	reading_direction : 'Right to Left',
+	genres:
+		[ 'Action',
+			'Comedy',
+			'Sci-Fi',
+			'Shounen']
+};
+
+
+
+export default {
+  mangas_count : 4404,
+  manga_name : manga.title,
+
+	chapter_count : 55,
+
+  manga
+
+}
