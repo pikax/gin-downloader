84163602d0af8ba4e9e7336d4f2022a330dce053
pikax <david-181@hotmail.com>
2017-03-06 13:48:07 +0000

    adding real tests

---
+++ test/mangafox/index.spec.js
@@ -0,0 +1,26 @@
+/**
+ * Created by rodriguesc on 06/03/2017.
+ */
+
+
+import site from './../../lib/mangafox'
+import config from "./_results";
+
+const expect = require('chai').expect;
+
+
+describe('MangaFox',()=> {
+
+
+    it('should get all mangas',done=>{
+
+        site.getAllMangas()
+            .then(mangas=>{
+                expect(mangas.length).to.be.greaterThan(config.mangas_count);
+            })
+            .then(done)
+            .catch(done);
+
+    })
+
+});
