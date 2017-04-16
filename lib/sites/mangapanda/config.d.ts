0c2ea5904cff5df7d284a47efc4f66a4b318ec89
pikax <david-181@hotmail.com>
2017-04-16 13:51:55 +0100

    build

--- lib/sites/mangapanda/config.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by rodriguesc on 05/03/2017.
- */
-import { SiteConfig } from "../../declarations";
-export declare const config: SiteConfig;
-export default config;
-export declare const debug: any;
-export declare const verbose: any;
