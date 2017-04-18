a746a462cf8c1d1559b78e8ded8c489070e990b8
pikax <david-181@hotmail.com>
2017-04-18 22:59:35 +0100

    fixing tests

--- test/_mock/SiteParser.ts
+++ test/_mock/SiteParser.ts
@@ -2,11 +2,18 @@
  * Created by pikax on 13/04/2017.
  */
 
-import {Chapter, MangaInfo, MangaSource, MangaXDoc, SiteParser} from "../../src/declarations";
+import {Chapter, FilteredResults, MangaInfo, MangaSource, MangaXDoc, SiteParser} from "../../src/declarations";
 
 import * as results from "./_results";
 
 export class Parser implements SiteParser {
+  filter(doc: MangaXDoc): Promise<FilteredResults> | FilteredResults {
+    return <FilteredResults>{
+      results: results.mangas,
+      page: 1,
+      total: 1
+    } ;
+  }
   mangas(doc: MangaXDoc): MangaSource[] | Promise<MangaSource[]> {
     return results.mangas;
   }
