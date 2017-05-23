1f16885c7c1d276286cb74ec7f469a086fa44d6a
pikax <david-181@hotmail.com>
2017-05-23 13:53:08 +0100

    batoto continuation, adding filters by genre

--- src/sites/batoto/filter.ts
+++ src/sites/batoto/filter.ts
@@ -1,7 +1,7 @@
 /**
  * Created by rodriguesc on 30/03/2017.
  */
-import {Genre, FilterCondition, FilterSupport, FilterStatus} from "../../declarations";
+import {Genre, FilterCondition, FilterSupport, FilterStatus, GenreCondition} from "../../declarations";
 import {config} from "./config";
 import {resolve} from "url";
 import {find} from "lodash";
@@ -102,6 +102,46 @@ const ordered = [
   Genre.Yuri,
 ];
 
+let dic: { [id: string]: string; } = {};
+dic[Genre.FourKoma] = "40";
+dic[Genre.Action] = "1";
+dic[Genre.Adventure] = "2";
+dic[Genre.AwardWinning] = "39";
+dic[Genre.Comedy] = "3";
+dic[Genre.Cooking] = "41";
+dic[Genre.Doujinshi] = "9";
+dic[Genre.Drama] = "10";
+dic[Genre.Ecchi] = "12";
+dic[Genre.Fantasy] = "13";
+dic[Genre.GenderBender] = "15";
+dic[Genre.Harem] = "17";
+dic[Genre.Historical] = "20";
+dic[Genre.Horror] = "22";
+dic[Genre.Josei] = "34";
+dic[Genre.MartialArts] = "27";
+dic[Genre.Mecha] = "30";
+dic[Genre.Medical] = "42";
+dic[Genre.Music] = "37";
+dic[Genre.Mystery] = "4";
+dic[Genre.Oneshot] = "38";
+dic[Genre.Psychological] = "5";
+dic[Genre.Romance] = "6";
+dic[Genre.SchoolLife] = "7";
+dic[Genre.SciFi] = "8";
+dic[Genre.Seinen] = "32";
+dic[Genre.Shoujo] = "35";
+dic[Genre.ShoujoAi] = "16";
+dic[Genre.Shounen] = "33";
+dic[Genre.ShounenAi] = "19";
+dic[Genre.SliceOfLife] = "21";
+dic[Genre.Smut] = "23";
+dic[Genre.Sports] = "25";
+dic[Genre.Supernatural] = "26";
+dic[Genre.Tragedy] = "28";
+dic[Genre.Webtoon] = "36";
+dic[Genre.Yaoi] = "29";
+dic[Genre.Yuri] = "31";
+dic[Genre.NoChapters] = "44";
 
 
 export const processFilter = (filter: FilterSupport) : {src: string, params: any} => {
@@ -112,40 +152,85 @@ export const processFilter = (filter: FilterSupport) : {src: string, params: any
   let fauthor = null;
   let fstatus = null;
   let fname = filter.name;
+  let nameCondition = null;
+  let authorCondition = null;
+  let genreCondition = null;
 
   if (search) {
     let nameFilter = search.name;
     if (nameFilter) {
       fauthor = nameFilter.name;
+      nameCondition = resolveCondition(nameFilter.condition); // not support equals just Starts|Ends|Contains
     }
 
     let authorFilter = search.author || search.artist;
     if (authorFilter) {
       fauthor = authorFilter.name;
+      authorCondition = resolveCondition(authorFilter.condition);
     }
 
     let statusFilter = search.status;
     if (statusFilter) {
       fstatus = resolveStatus(statusFilter);
     }
+
+    let genreFilter = search.genre;
+    if(genreFilter) {
+      genres = genreFilter.inGenres;
+      outGenres = genreFilter.outGenres;
+      genreCondition = resolveGenreCondition(genreFilter.condition);
+    }
   }
 
-  const mangaName = `mangaName=${fname || ""}`;
+  const mangaName = `name=${fname || ""}`;
+  const nameCond = nameCondition && `name_cond=${nameCondition}`;
   const authorArtist =  `authorArtist=${(fauthor || "")}`;
-  const genreFilter = ordered.map(x => inOutGenre(x, genres, outGenres)).map(x => `genres=${x}`).join("&");
+  const authorCond = authorCondition && `name_cond=${authorCondition}`;
+
+  const genreFilter = ordered.map(x => inOutGenre(x, genres, outGenres)).map(x => `genres=${x}`).join(";");
+  const genreCon =( genreCondition && `genre_cond=${genreCondition}` ) || "genre_cond=and"; // todo change me
   const status = `status=${(fstatus || "")}`;
 
-  return {src: resolve(config.site, "/AdvanceSearch"),
-    params: [mangaName, authorArtist, genreFilter, status].join("&")
+  return {src: resolve(config.site, "/search"),
+    params: [mangaName, nameCond, authorArtist, authorCond, genreFilter, genreCon, status].join("&")
   };
 };
+
+
+function resolveCondition(condition: FilterCondition) {
+  switch (condition) {
+    case FilterCondition.Contains:
+      return "c";
+    case FilterCondition.EndsWith:
+      return "e";
+    case FilterCondition.StartsWith:
+      return "s";
+    case FilterCondition.Equal: // not supported for name
+      return "is";
+
+    default:
+      return "c";
+  }
+}
+
+function resolveGenreCondition(condition: GenreCondition) {
+  switch (condition) {
+    case GenreCondition.And:
+      return "and";
+    case GenreCondition.Or:
+      return "or";
+
+    default:
+      return "and";
+  }
+}
 
 function resolveStatus(status: FilterStatus) {
   switch (status) {
     case FilterStatus.Ongoing:
-      return "Ongoing";
+      return "i";
     case FilterStatus.Complete:
-      return "Complete";
+      return "c";
     default:
       return null;
   }
@@ -153,10 +238,10 @@ function resolveStatus(status: FilterStatus) {
 
 function inOutGenre(genre: Genre, inGenre: Genre[], outGenre: Genre[]) {
   if (inGenre && inGenre.indexOf(genre) > -1) {
-    return 1;
+    return `i${dic[genre]}`;
   }
   if (outGenre && outGenre.indexOf(genre) > -1) {
-    return 2;
+    return `e${dic[genre]}`;
   }
   return 0;
 }
