ec6920d4a453a70bf62e16b9852b169cddd8fd16
pikax <carlos.rodrigues@iplicit.com>
2017-06-07 14:11:21 +0100

    fix batoto info test

--- test/sites/batoto/_results.ts
+++ test/sites/batoto/_results.ts
@@ -12,7 +12,6 @@ let manga = { image: "http://img.bato.to/forums/uploads/501378b77edcdef486ef2260
       "Action",
       "Adventure",
       "Comedy",
-      "Drama",
       "Historical",
       "Sci-fi",
       "Shounen",
