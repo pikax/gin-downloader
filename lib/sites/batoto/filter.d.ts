84f5c3484bf0ce8a6f8b662975c7dd6bf46701f9
pikax <david-181@hotmail.com>
2017-06-10 16:44:48 +0100

    replacing lib to dist folder, now is publishing single file

--- lib/sites/batoto/filter.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Created by rodriguesc on 30/03/2017.
- */
-import { FilterSupport } from "../../declarations";
-export declare const processFilter: (filter: FilterSupport) => {
-    src: string;
-};
