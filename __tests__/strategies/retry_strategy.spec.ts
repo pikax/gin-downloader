a716e5bbcd688be9b2e8f37ec1cdb7c4249a0d0c
pikax <david-181@hotmail.com>
2018-04-22 12:06:03 +0100

    retry_strategy test

--- __tests__/strategies/retry_strategy.spec.ts
+++ __tests__/strategies/retry_strategy.spec.ts
@@ -1,12 +1,42 @@
-import {RequestRetryStrategy} from "../../build/strategies/retry_strategy";
+import {RequestRetryStrategy} from "../../src/strategies/retry_strategy";
+
+const requestRetry: any = require("requestretry");
+
+jest.mock("requestretry");
 
 describe("retry strategy", () => {
 
 
-    it('fail', () => {
+    beforeEach(() => {
+        requestRetry.mockClear();
+    });
+
+    it("should call requestretry", async () => {
+        const strategy = new RequestRetryStrategy();
 
 
-        expect(1).toBe(2);
-    })
+        const uri = "test";
+        const response = await strategy.request({uri});
+
+        expect(requestRetry.mock.calls.length).toBe(1);
+        expect(requestRetry.mock.calls[0][0].uri).toBe(uri);
+    });
+
+
+    it("should class requestretry with the params", async () => {
+        const maxAttempts = 50;
+        const retryDelay = 200;
+
+        const strategy = new RequestRetryStrategy(maxAttempts, retryDelay);
+
+        const uri = "test";
+
+        const response = await strategy.request({uri});
+
+        expect(requestRetry.mock.calls.length).toBe(1);
+        expect(requestRetry.mock.calls[0][0].uri).toBe(uri);
+        expect(requestRetry.mock.calls[0][0].maxAttempts).toBe(maxAttempts);
+        expect(requestRetry.mock.calls[0][0].retryDelay).toBe(retryDelay);
+    });
 
 });
