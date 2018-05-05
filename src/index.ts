e8b61c713cdbff32fc05b2da2873aad20532cb14
pikax <david-181@hotmail.com>
2018-05-05 09:16:10 +0100

    Add 'Hajime-chan is No. 1' to the invalid list

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 export * from "./interface";
 
 import {GinBuilder} from "./builder";
-export {GinBuilder} from "./builder";
+export {GinBuilder, MangaHereBuilder} from "./builder";
 
 export * from "./enum";
 export * from "./filter";
 
 
+export  * from "./parser";
+
 export const gindownloader = new GinBuilder();
+
+
+
+export {MangaHereConfig} from "./manga/mangahere/config";
+export {RequestRetryStrategy} from "./strategies/retry_strategy";
+export {MangaHereParser} from "./manga/mangahere/parser";
+export {mangahereLogger} from "./manga/mangahere/logger";
+export {MangaHereGenre} from "./manga/mangahere/genre";
+export {MangaRequestResult} from "./util/mangaRequestResult";
+export {RetryRequestFactory} from "./factories/retryRequest";
